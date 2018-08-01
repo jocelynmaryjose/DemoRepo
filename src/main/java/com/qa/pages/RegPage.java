a79cd57245dcd0c6cc12a37a500c54b21f0d7501
jocelynmaryjose <jocelynmaryjose@gmail.com>
2018-08-01 11:23:39 +0530

    my first git commit for selenium code

---
+++ src/main/java/com/qa/pages/RegPage.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class RegPage {
+
+	public void regpage() {
+		System.out.println("Regstration page");
+	}
+}
---
+++ src/test/java/com/qa/tests/LoginTests.java
@@ -0,0 +1,8 @@
+package com.qa.tests;
+
+public class LoginTests {
+
+	public void logintes() {
+		System.out.println("login tests func");
+	}
+}
