ce70fd0ba0bb78fc3529edcae5faddd5e0ef5b39
jocelynmaryjose <jocelynmaryjose@gmail.com>
2018-08-01 12:14:58 +0530

    added homepage and modified login

---
+++ src/main/java/com/qa/pages/Homepage.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class Homepage {
+	public void home() {
+		System.out.println("Home page test");
+	}
+}
+
--- src/main/java/com/qa/pages/Loginpage.java
+++ src/main/java/com/qa/pages/Loginpage.java
@@ -5,5 +5,7 @@ public class Loginpage {
 	public void loginpage() {
 		System.out.println("Login page");
 	}
-	
+	public void npage() {
+		System.out.println("test page");
+	}
 }
